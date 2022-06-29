dc87651376fcfa566c56f5f3f167ef2e45d97b63
Minhaz <minhaz1217@gmail.com>
2022-06-29 20:50:38 +0600

    super basic integration test working

--- IntegrationTestingDummyApp.Tests/BasicTests.cs
+++ IntegrationTestingDummyApp.Tests/BasicTests.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace IntegrationTestingDummyApp.Tests
 {
-    public class BasicTests
+    public class BasicTests : IClassFixture<WebApplicationFactory<Program>>
     {
+        private readonly WebApplicationFactory<Program> _factory;
+        public BasicTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+
+        [Theory]
+        [InlineData("/WeatherForecast")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+        }
+
+
+        // Build the client inside the test run.
+        [Fact]
+        public async Task HelloWorldTest()
+        {
+            var application = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    // ... Configure test services
+                });
+
+            var client = application.CreateClient();
+            //...
+        }
     }
 }
